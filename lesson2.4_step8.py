793b2a2e1f391270eb45c381b1a2006c6d47305b
EvgBelozeroff <yevgeniy.belozerov@gmail.com>
2021-09-28 22:34:24 +0700

    homework from previous module

--- lesson2.4_step8.py
+++ lesson2.4_step8.py
@@ -3,12 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import math
+import time
 
 browser = webdriver.Firefox()
 
 try:
     browser.get("http://suninjuly.github.io/explicit_wait2.html")
-    button = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), '100'))
+    button = WebDriverWait(browser, 20).until(EC.text_to_be_present_in_element((By.ID, "price"), '100'))
 
     browser.find_element_by_id('book').click()
     inputValue = browser.find_element_by_id('input_value')
@@ -26,4 +27,5 @@ try:
     submit.click()
 
 finally:
+    time.sleep(5)
     browser.quit()
