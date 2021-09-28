d97b333affba46d0d5c9ccfcd1590a831479c5c1
EvgBelozeroff <yevgeniy.belozerov@gmail.com>
2021-09-28 22:19:11 +0700

    Добавил решение одной из задач с курса для тренировки

--- lesson3_step4.py
+++ lesson3_step4.py
@@ -1,1 +1,29 @@
-print(12)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import math
+
+browser = webdriver.Firefox()
+
+try:
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+    button = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), '100'))
+
+    browser.find_element_by_id('book').click()
+    inputValue = browser.find_element_by_id('input_value')
+    x = inputValue.text
+
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+    y = calc(x)
+    answerForm = browser.find_element_by_id('answer')
+    submit = browser.find_element_by_id('solve')
+    answerForm.send_keys(y)
+    submit.click()
+
+finally:
+    browser.quit()
