f28f9c3c6f10c269b0f096ff3c03d240b4f14b65
EvgBelozeroff <yevgeniy.belozerov@gmail.com>
2021-10-04 22:40:56 +0700

    Добавил сообщеие в assert, комментарии к коду

--- test_items.py
+++ test_items.py
@@ -1,7 +1,10 @@
 # Проверка наличия кнопки добавления в корзину на странице товара
 def test_presence_add_cart_button_diff_languages(browser):
-    url = 'http://selenium1py.pythonanywhere.com//catalogue/coders-at-work_207/'
+    url = 'http://selenium1py.pythonanywhere.com//catalogue/coders-at-work_206/'
     browser.get(url)
     add_to_cart_button_elements = browser.find_elements_by_css_selector('.btn-add-to-basket')
     count_add_to_cart_button_elements = len(add_to_cart_button_elements)
-    assert count_add_to_cart_button_elements == 1
+    # для assert выбрано сравнение на равенство с единицей, а не > 0, т.к. если элементов будет больше, чем один,
+    # то с тестом что-то не так, он должен упасть
+    assert count_add_to_cart_button_elements == 1, 'Кнопка добавления в корзину отсутстует на странице товара (либо ' \
+                                                   'кнопок больше, чем 1) '
