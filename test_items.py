03e8d4d944ca321ce1b69599324e6e0c5b048721
EvgBelozeroff <yevgeniy.belozerov@gmail.com>
2021-10-04 22:43:35 +0700

    исправил урл в тесте

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 # Проверка наличия кнопки добавления в корзину на странице товара
 def test_presence_add_cart_button_diff_languages(browser):
-    url = 'http://selenium1py.pythonanywhere.com//catalogue/coders-at-work_206/'
+    url = 'http://selenium1py.pythonanywhere.com//catalogue/coders-at-work_207/'
     browser.get(url)
     add_to_cart_button_elements = browser.find_elements_by_css_selector('.btn-add-to-basket')
     count_add_to_cart_button_elements = len(add_to_cart_button_elements)
